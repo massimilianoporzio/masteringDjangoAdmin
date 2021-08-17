50d26b6bebbf7ac3a0e92b1ea9b26349398c1a3f
Massimiliano <massimiliano.porzio@gmail.com>
2021-08-17 21:41:32 +0200

    messo custom filter e altre custom action (usando anche una view)

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -1,10 +1,44 @@
-from datetime import datetime
+import csv
+from datetime import datetime, timedelta
 
 from django.contrib import admin
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.utils.html import format_html
+from django.utils import timezone
 
 from sample_app.models import *
 
+
+class QuestionPublishedListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = ('Published questions')
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'pub_date'
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('Published', ('Published questions')),
+            ('Unpublished', ('Unpublished questions')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == 'Published':
+            return queryset.filter(pub_date__lt=datetime.now())
+        if self.value() == 'Unpublished':
+            return queryset.filter(pub_date__gte=datetime.now())
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
@@ -21,6 +55,38 @@ class AuthorAdmin(admin.ModelAdmin):
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
+    class QuestionPublishedListFilter(admin.SimpleListFilter):
+        # Human-readable title which will be displayed in the
+        # right admin sidebar just above the filter options.
+        title = ('Published questions')
+        # Parameter for the filter that will be used in the URL query.
+        parameter_name = 'pub_date'
+
+        def lookups(self, request, model_admin):
+            """
+            Returns a list of tuples. The first element in each
+            tuple is the coded value for the option that will
+            appear in the URL query. The second element is the
+            human-readable name for the option that will appear
+            in the right sidebar.
+            """
+            return (
+                ('Published', ('Published questions')),
+                ('Unpublished', ('Unpublished questions')),
+            )
+
+        def queryset(self, request, queryset):
+            """
+            Returns the filtered queryset based on the value
+            provided in the query string and retrievable via
+            `self.value()`.
+            """
+            if self.value() == 'Published':
+                return queryset.filter(pub_date__lt=datetime.now(tz=timezone.get_default_timezone()))
+            if self.value() == 'Unpublished':
+                return queryset.filter(pub_date__gte=datetime.now(tz=timezone.get_default_timezone()))
+
+
     empty_value_display = 'Unknown'
     fieldsets = [
         ("Question information", {
@@ -42,10 +108,16 @@ class QuestionAdmin(admin.ModelAdmin):
     # list_editable = ('refAuthor',)
     list_per_page = 20
 
+
+
+
     #performance
     list_select_related = ('refAuthor',)
 
-    list_filter = ('refAuthor',)
+    list_filter = (
+                    QuestionPublishedListFilter ,  # custom filter
+                    'refAuthor',
+                   )
     # autocomplete_fields = ['refAuthor']
     # OPPURE
     raw_id_fields = ('refAuthor',)
@@ -66,6 +138,54 @@ class QuestionAdmin(admin.ModelAdmin):
 
     goToChoices.short_description = 'Choices'
     goToChoices.allow_tags = True
+
+    # custom action
+    def make_published(modeladmin, request, queryset):
+        queryset.update(pub_date=datetime.now(tz=timezone.get_default_timezone()) - timedelta(days=1))
+
+    # per salvare i campi in un csv (ma non i campi manytomany o ForeignKey
+    def export_to_csv(modeladmin, request, queryset):
+        opts = modeladmin.model._meta
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; \
+            filename={}.csv'.format(opts.verbose_name)
+        writer = csv.writer(response)
+        fields = [field for field in opts.get_fields()
+                  if not field.many_to_many and not field.one_to_many]
+        # Write a first row with header information
+        writer.writerow([field.verbose_name for field in fields])
+        # Write data rows
+        for obj in queryset:
+            data_row = []
+            for field in fields:
+                value = getattr(obj, field.name)
+                if isinstance(value, datetime):
+                    value = value.strftime('%d/%m/%Y %H:%M')
+                data_row.append(value)
+            writer.writerow(data_row)
+        return response
+
+    export_to_csv.short_description = 'Export to CSV'
+
+    #custom action che manda su una view e poi esegue l'azione con l'input che arriav da quella pagina
+    # in realtà è semplicemente una View usata come action
+    def make_published_custom(self, request, queryset):
+        if 'apply' in request.POST:
+            # The user clicked submit on the intermediate form.
+            # # Perform our update action:
+            queryset.update(pub_date=datetime.now(tz=timezone.get_default_timezone()) - timedelta(days=1))
+            # Redirect to our admin view after our update has
+            # completed with a nice little info message saying
+            # our models have been updated:
+            self.message_user(request,
+                              "Changed to published on {} questions".format(queryset.count()))
+            return HttpResponseRedirect(request.get_full_path())
+
+        return render(request, 'admin/custom_makepublished.html', context={'questions': queryset})
+
+    actions = [make_published,export_to_csv,make_published_custom]
+
+    make_published.short_description = "Mark selected questions as published"
 
 
 # Register your models here.
