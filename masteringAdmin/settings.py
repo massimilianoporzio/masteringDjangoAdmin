7806bea65f9f24fcaee60ea42e64e32504bcd901
Massimiliano <massimiliano.porzio@gmail.com>
2021-08-18 18:11:02 +0200

    custom admin with static CHART js

--- masteringAdmin/settings.py
+++ masteringAdmin/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"sample_app/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- sample_app/admin.py
+++ sample_app/admin.py
@@ -2,6 +2,10 @@ import csv
 from datetime import datetime, timedelta
 
 from django.contrib import admin
+from django.core.serializers import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Count
+from django.db.models.functions import TruncDay
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils.html import format_html
@@ -46,6 +50,25 @@ class QuestionPublishedListFilter(admin.SimpleListFilter):
 
 # @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new authors per day
+        chart_data = (
+            Author.objects.annotate(date=TruncDay("updatedDate"))
+                .values("date")
+                .annotate(y=Count("id"))
+                .order_by("-date")
+        )
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        print("Json %s" % as_json)
+        extra_context = extra_context or {"chart_data": as_json}
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
+
+
     empty_value_display = 'Unknown'
     fieldsets = [
         ("Author Info", {'fields': ['name','createdDate','updatedDate']}),
